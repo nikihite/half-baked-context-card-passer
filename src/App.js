98b93a69465efa35ea9d1bbdd1199f07cb4bf69b
Niki Hite <nikihite@comcast.net>
2022-07-14 14:35:14 -0700

    commented out test

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <section>
+        card dealer:
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
         <Player player={1} hand={playerOneHand}/>
         <Player player={2} hand={playerTwoHand}/>
